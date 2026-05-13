590ee52e996f230c0947367d32b7afb03b33eb88
Sharon Moegi <sharamoegi@gmail.com>
2026-05-13 20:26:40 +0300

    Write About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function About() {
+    return(
+        <section>
+            <p>
+                Welcome to Vibe Kitchen: Cook Based on Your Mood
+                Ever stared at a full fridge and thought, "I have no idea what I want, but I know how I feel"?
+                We get it. The internet is drowning in recipes, but most of them demand that you already have a plan. Scrolling through endless blogs and ingredient lists shouldn't feel like a chore. When you're tired, stressed, or feeling celebratory, the last thing you need is decision fatigue.
+                Vibe Kitchen is here to bring the joy back to your stove by flipping the script on meal discovery. Instead of asking "What’s for dinner?", we ask "How are you feeling?"
+            </p>
+        </section>
+    )
+}
