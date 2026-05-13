d6c65d1fbf9f184f6ba2b2b8789fe22bd048eb65
Sharon Moegi <sharamoegi@gmail.com>
2026-05-14 00:03:56 +0300

    import components

---
+++ src/Pages/DiscoverPage.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+import { SlidersHorizontal } from 'lucide-react';
+import MoodSelector from '../components/MoodSelector.jsx';
+import SearchBar from '../components/SearchBar.jsx';
+import FilterPanel from '../components/FilterPanel.jsx';
+import RecipeGrid from '../components/RecipeGrid.jsx';
