e98ca7e0e964be792dd582de9e4f23f4265b316e
Sharon Moegi <sharamoegi@gmail.com>
2026-05-13 21:22:16 +0300

    implement handleClear to reset input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,4 +3,9 @@ import { Search, X } from "lucide-react";
 
 export default function SearchBar({value, onChange}) {
     const inputRef = useRef(null);
+    // Clear SearchBar
+    function handleClear() {
+        onChange("");
+        inputRef.current?.focus();
+    }
 }
