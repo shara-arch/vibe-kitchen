80ae36cb5729a40f70fdf9b9e866689f79913567
Sharon Moegi <sharamoegi@gmail.com>
2026-05-13 21:43:26 +0300

    Create Filter Options Object

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,1 +1,6 @@
 import { SlidersHorizontal } from "lucide-react";
+
+const FILTER_OPTIONS = {
+  'Dietary': ['High Protein', 'Low Carb', 'Vegetarian', 'Vegan'],
+  'Allergies': ['No Nuts', 'No Dairy', 'No Eggs', 'No Soy', 'No Fish'],
+}
