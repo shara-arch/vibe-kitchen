735ecd91c7154bc3a06f80d5aeb1e1e75b7e5a59
Sharon Moegi <sharamoegi@gmail.com>
2026-05-14 00:49:07 +0300

    Compose App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import DiscoverPage from './pages/DiscoverPage.jsx';
+
+export default function App(){
+    return (
+        <div>
+        <div className='min-h-screen bg-stone-50'>
+            <Navbar/>
+            <Routes>
+            <Route path ="/" element={<DiscoverPage/>} />
+            </Routes>
+        </div>
+        </div>
+    )
+}
